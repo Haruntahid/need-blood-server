fc2b63970e6085b753cf600e4b365ed03674ac70
haruntahid <tahidtaha997@gmail.com>
2024-06-04 00:33:57 +0600

    store donation req on database

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const usersCollection = client.db("needBlood").collection("all-users");
+    const districtsCollection = client.db("needBlood").collection("districts");
+    const upazilasCollection = client.db("needBlood").collection("upazilas");
+    const donationRequestCollection = client
+      .db("needBlood")
+      .collection("donationReq");
 
     app.get("/", async (req, res) => {
       res.send("server is running");
@@ -43,6 +48,41 @@ async function run() {
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    });
+
+    // get all users data
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get a specific user based on email
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
+    // donation request => on donor post
+    app.post("/donation-request", async (req, res) => {
+      const donation = req.body;
+      const result = await donationRequestCollection.insertOne(donation);
+      res.send(result);
+    });
+
+    // get all districts
+    app.get("/districts", async (req, res) => {
+      const result = await districtsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get all upazilas based on district id
+    app.get("/upazilas", async (req, res) => {
+      const districtId = req.query.district_id;
+      const query = { district_id: districtId };
+      const filteredUpazilas = await upazilasCollection.find(query).toArray();
+      res.send(filteredUpazilas);
     });
 
     // Send a ping to confirm a successful connection
